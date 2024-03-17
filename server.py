7f7ae82e298c0404112a745b60ad6b506eb9dd0c
Dorian <adonosoa@hotmail.com>
2024-03-16 22:11:21 -0300

    fixs error in server users and templates

--- server.py
+++ server.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, redirect,request
 from users import User
+
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def index():
     return redirect('/users')
 
-@app.route("/users")
+@app.route('/users')
 def users():
     users = User.get_all()
     return render_template("users.html", usuarios =users)
@@ -16,20 +17,41 @@ def users():
 def new():
     return render_template("new.html")
 
-#guardar
-@app.route('/user/create', methods=['POST'])
+#agregar
+@app.route('/user/create',methods=['POST'])
 def create():
     print(request.form)
     User.save(request.form)
     return redirect('/users')
 
+@app.route('/user/edit/<int:id>')
+def edit(id):
+    data={
+        "id":id
+    }
+    return render_template("edit_user.html", persona=User.get_one(data))
+
 #mostrar
 @app.route('/user/show/<int:id>')
 def show(id):
-    data = {
+    data ={
         "id":id
     }
     return render_template("show_user.html", persona=User.get_one(data))
+
+
+@app.route('/user/update',methods=['POST'])
+def update():
+    User.update(request.form)
+    return redirect('/users')
+
+@app.route('/user/destroy/<int:id>')
+def destroy(id):
+    data={
+        'id':id
+    }
+    User.destroy(data)
+    return redirect('/users')
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
--- users.py
+++ users.py
@@ -1,11 +1,16 @@
 from mysqlconnection import connectToMySQL
 class User:
     def __init__(self, data):
+        self.id =data['id']
         self.first_name =data['first_name']
         self.last_name =data['last_name']
         self.email =data['email']
         self.created_at =data['created_at']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
     
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
     @classmethod
     def save(cls,data):
         query = "INSERT INTO users(first_name, last_name, email) VALUES(%(first_name)s,%(last_name)s, %(email)s);"
@@ -21,3 +26,21 @@ class User:
         for u in results: 
             users.append(cls(u))
         return users
+    
+    @classmethod
+    def get_one(cls, data):
+        query = "SELECT * FROM users WHERE id = %(id)s;"
+        print("LLEGA ANTES DESPUES LEER LA QUERY =====D ")
+        result = connectToMySQL('users').query_db(query,data)
+        return cls(result[0])
+    
+    @classmethod
+    def update(cls,data):
+        query = "UPDATE users SET first_name=%(first_name)s, last_name=%(last_name)s"
+        return connectToMySQL('users').query_db(query,data)
+    
+    @classmethod
+    def destroy(cls,data):
+        query= "DELETE FROM users WHERE id=%(id)s"
+        return connectToMySQL('users').query_db(query,data)
+    
